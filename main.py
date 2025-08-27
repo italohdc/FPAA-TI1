7e633a316ca9cb8b475f1f6dbe968f1498c62be5
Italo Lelis <italohdc@gmail.com>
2025-08-27 15:01:18 -0300

    feat: create boilerplate (contract) for karatsuba function

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import typer
 
 
-def main(name: str):
-  print(f"Hello {name}")
+def karatsuba(n1: int, n2: int) -> int:
+  raise NotImplementedError("Implementar o algoritmo de Karatsuba")
+
+
+def main(number1: int, number2: int):
+  result = karatsuba(number1, number2)
+  print(f"O resultado da multiplicação de {number1} e {number2} é {result}")
 
 
 if __name__ == "__main__":
